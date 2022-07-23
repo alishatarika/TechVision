adc4cbbf07e893623915c11412b9670905f80350
alishatarika <alisha.tarikaa@gmail.com>
2022-07-23 16:22:44 +0530

    add cprogram

---
+++ CPrograms/loops.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{ 
+    int number;
+    printf("Enter a number");
+    scanf("%d, &number");
+    if ( number>34)
+     
+    printf( "Enter a number greater than 54");
+
+    else( number<78)
+
+     ;printf("Enter a number less than 100");
+
+    return 0;
+
+}
--- CPrograms/practice.c
+++ CPrograms/practice.c
@@ -1,12 +1,7 @@
-#include <stdio.h>
-int main( ) { 
-int num ;
-printf ( "Enter a number less than 10 " ); 
-scanf ( "%d", &num );
- if ( num <= 10 ){  
-printf ( "Number is less than 10");}
-else{
-printf("Number is greater than 10");
-}
-return 0; 
+#include<stdio.h>
+int add (int a, int b){
+    printf("a=5");
+    printf("b=9");
+
+    return 0;
 }
