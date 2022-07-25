cd28a8e423441bcfc3223af541b8e8a27c640d75
alishatarika <alisha.tarikaa@gmail.com>
2022-07-25 16:56:24 +0530

    add simpleinterest program

---
+++ CPrograms/Simpleinterest.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{ 
+  
+
+ int p; 
+  int n ; 
+ float r, si ; 
+ p = 652 ; 
+ n = 397 ; 
+ r = 67 ; 
+ 
+ si = p * n * r / 100 ; 
+ printf ( "%f" , si ) ;  
+
+ return 0;
+}
--- CPrograms/table.c
+++ CPrograms/table.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 
-
 int main()
-{
 
-  int n;
   printf("Enter a number: ");
   scanf("%d", &n);
-  n = 10 * 5;
-  printf( "%d", n);
-  n=2;
-  n=3;
-  printf( "%d", n);
-
+  int n ;
+  {
+    printf("%d *1 =%d" );
+    printf("%d *2 = %d");
+    printf("%d *3 = %d");
+    printf("%d *4 = %d") ;
+    printf("%d *5 = %d");
+    printf("%d *6 = %d");
+    printf("%d *7 = %d");
+    printf("%d *8 = %d") ;
+    printf("%d *9 = %d" );
+    printf("%d *10 = %d" );
   
 
   return 0;
