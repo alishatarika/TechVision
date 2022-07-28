6351d65badbad46c7e1c8b7d796a8716b578086d
alishatarika <alisha.tarikaa@gmail.com>
2022-07-28 19:41:17 +0530

    add countingprogram

---
+++ CPrograms/counting.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int counting;
+
+    
+    
+    for(counting=1 ;counting <=100 ;counting = counting + 1)
+    printf("%d\n", counting);
+
+    return 0;
+}
