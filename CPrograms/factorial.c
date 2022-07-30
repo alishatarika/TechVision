d20fbbdfff286d2ed66e3387b3462702b7ddfcc6
alishatarika <alisha.tarikaa@gmail.com>
2022-07-30 18:48:51 +0530

    add factorial

---
+++ CPrograms/factorial.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int factorial = 1;
+    int num;
+    int i;
+    printf("Enter a number to find its factorial :");
+    scanf("%d" , &num);
+
+    
+    for(i=1;i<=num ;i++)
+     factorial =factorial * i;
+    
+    printf("%d!=%d \n", num , factorial);
+
+    return 0;
+}
