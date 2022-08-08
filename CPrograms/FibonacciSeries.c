8937d59a638df23dfaa9ffac4ff05c1c0d49d3e6
alishatarika <alisha.tarikaa@gmail.com>
2022-08-08 15:40:12 +0530

    add fibonacciseries

--- CPrograms/FibonacciSeries.c
+++ CPrograms/FibonacciSeries.c
@@ -3,19 +3,22 @@
 int main()
 {
     int n;
+    printf("Enter the value of n : ");
+    scanf("%d", &n);
+
     int n1 = 0;
     int n2 = 1;
     int sum;
-    printf("Enter the value of n : ");
-    scanf("%d", &n);
-    printf("%d " , n1 );
     
+    printf("%d ", n1 );
+
     for (int i = 0; i <= n; i++)
     {
-        
+
         n1 = n2;
         n2 = sum;
         sum = n1 + n2;
-        printf("  %d", sum);
+        printf(" %d " ,sum);
     }
+    return 0;
 }
