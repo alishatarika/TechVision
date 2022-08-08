6b59469f2fbe16fe99702c39bff99eaea5c677d2
alishatarika <alisha.tarikaa@gmail.com>
2022-08-08 20:17:16 +0530

    add starprogram

--- CPrograms/Star.c
+++ CPrograms/Star.c
@@ -5,12 +5,12 @@ int main()
  printf("Enter a number ");
  scanf("%d" , &n);
   
-for(int i = 1; i <= n  ; i++)
+for(int i = n ; i >=1  ; i--)
 {
 
- for( int j = 1; j <= i ; j++)
+ for(int j = 1; j <= i ; j++)
  {
- printf("*");
+ printf(" * ");
  }
  printf("\n");
  
