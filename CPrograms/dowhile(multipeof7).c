e73a70467857eff945b8b2cb22ea323186faad30
alishatarika <alisha.tarikaa@gmail.com>
2022-11-06 20:35:13 +0530

    addprogram

---
+++ CPrograms/dowhile(multipeof7).c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    do{
+        printf("enter number : ");
+    scanf("%d",&n);
+    printf("%d\n",n);
+    if("%d",n%7==0){
+        break;
+    } }while   (1);
+
+    printf("t");
+    return 0;
+}
