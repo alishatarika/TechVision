171a0c1409db7b2f983642506933eb6babef0cd4
alishatarika <alisha.tarikaa@gmail.com>
2022-11-07 09:53:52 +0530

    addfunctions

--- CPrograms/factorial.c
+++ CPrograms/factorial.c
@@ -9,9 +9,9 @@ int main()
     scanf("%d" , &num);
 
     
-    for(i=1;i<=num ;i++)
+    for(i=1;i<=num ;i++){
      factorial =factorial * i;
-    
+    }
     printf("%d=%d \n", num , factorial);
 
     return 0;
