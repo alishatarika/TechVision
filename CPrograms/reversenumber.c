90445997e15d947cf36b5d92c57ca6cebaa57079
alishatarika <alisha.tarikaa@gmail.com>
2022-11-16 23:14:34 +0530

    addreversecprogram

---
+++ CPrograms/reversenumber.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+// 12345
+// 54321
+{
+    int number;
+    printf("Enter a five digit number ");
+    scanf("%d", &number);
+
+    int number1 = number % 10;
+    printf("%d", number1);
+    number1 = number / 10;
+    printf("%d", number1 % 10);
+    int number2 = number1 / 10;
+    printf("%d", number2 % 10);
+    int number3 = number2 / 10;
+    printf("%d", number3 % 10);
+    int number4 = number3 / 10;
+    printf("%d", number4 % 10);
+
+    return 0;
+}
