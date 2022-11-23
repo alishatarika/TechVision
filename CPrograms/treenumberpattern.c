3dd69b66ab3892654b30f824a5c723de719efd4b
alishatarika <alisha.tarikaa@gmail.com>
2022-11-23 23:44:08 +0530

    addcprogram

--- CPrograms/treenumberpattern.c
+++ CPrograms/treenumberpattern.c
@@ -1,23 +1,31 @@
 #include<stdio.h>
 int main()
 {
+
     int n;
+    int i,j,k;
+    int l=1;
+    
     printf("Enter a number ");
     scanf("%d",&n);
     //     1
     //    2 3
     //   4 5 6
     //  7 8 9 10
-    for(int i=1;i<=n;i++)
+    for( i = 1; i <= n; i++)
     {
-        for (int j=n;j>i;j--)
+      for(k=n; k>i; k--)
         {
-        
         printf(" ");
         }
         
-        for(int k=1;k<=i;k++)
-        printf("%d",i);
+        for(j=1; j<=i ;j++)
+        {
+         l++;
+        printf(" %d",l);
+        }
+        
+
         printf("\n");
         
     }
