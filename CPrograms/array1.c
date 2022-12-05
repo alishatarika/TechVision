e7352349d63d86900ab419ba7176b2a7af988674
alishatarika <alisha.tarikaa@gmail.com>
2022-12-06 00:08:18 +0530

    addarrayprogram

---
+++ CPrograms/array1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+ int marks[4];
+ printf("Enter the marks of physics ");
+ scanf("%d",&marks[0]);
+printf("Enter the marks of hindi ");
+ scanf("%d",&marks[1]);
+printf("Enter the marks of english ");
+ scanf("%d",&marks[2]);
+printf("Enter the marks of maths ");
+ scanf("%d",&marks[3]);
+printf("%d,%d,%d,%d",marks[0],marks[1],marks[2],marks[3]);
+return 0;
+}
