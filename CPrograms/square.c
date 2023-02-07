5ba4a2b10b91bf94682811d9d144f04bfed377e4
alishatarika <alisha.tarikaa@gmail.com>
2023-02-07 23:38:38 +0530

    addcprogram

---
+++ CPrograms/square.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void square(int n);
+int main()
+{
+    int number;
+    
+    printf("enter a number");
+    scanf("%d",&number);
+    square(number);
+    printf("number is %d",number);
+    return 0;
+
+}
+void square (int n)
+{
+    n=n*n;
+    printf("square is %d",n);
+}
