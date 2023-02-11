d520d87fd181a7d161c0b77a5f505a48faace9f5
alishatarika <alisha.tarikaa@gmail.com>
2023-02-11 19:40:07 +0530

    addcprogram

---
+++ CPrograms/stonepapergame.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+int me ;
+int computer;
+
+
+printf("\n1. Select rock");
+printf("\n2. Select paper");
+printf("\n3.Select scissor");
+printf("\n4.Exit");
+printf("\n Enter your choice from(1-4) ");
+scanf("%d",&me);
+
+computer=rand() % 3;
+
+if(me==4){
+exit(1);}
+else if(me==1 && computer==2){
+    printf("\n COMPUTER WON \n");
+}
+else if(me==1 && computer==3){
+    printf("\n YOU WON \n");
+}else if(me==2 && computer==1){
+    printf("\n YOU WON \n");
+}else if(me==2 && computer==3){
+    printf("\n  COMPUTER WON \n");
+}
+else if(me==3 && computer==1){
+    printf("\n COMPUTER WON \n");
+}else if(me==3 && computer==2){
+    printf("\n YOU WON \n");
+}
+else{
+    printf("GAME DRAW \n");
+}
+return 0;
+
+}
