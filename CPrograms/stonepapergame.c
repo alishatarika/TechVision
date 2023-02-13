a734aceecc81bc966e71fc74094def37dd52b8c1
alishatarika <alisha.tarikaa@gmail.com>
2023-02-13 22:46:00 +0530

    addcprogram

--- CPrograms/stonepapergame.c
+++ CPrograms/stonepapergame.c
@@ -9,6 +9,8 @@ int main()
     int PAPER=2;
     int SCISSOR=3;
     int EXIT=4;
+    int YOUR_SCORE=0;
+    int COMPUTER_SCORE=0;
     srand(time(NULL));
     printf("\n1. Select rock");
     printf("\n2. Select paper");
@@ -31,38 +33,49 @@ int main()
         else if (me == ROCK && computer == PAPER)
         {
             printf("\nYour choice is rock ,Computer choice is paper  **paper beat rock ");
-            printf("\n COMPUTER WON \n");
+            printf("\n YOU LOSS \n");
+            COMPUTER_SCORE++;
         }
         else if (me == ROCK && computer == SCISSOR)
         {
             printf("\nYour choice is rock ,Computer choice is scissor  **rock beat scissor ");
             printf("\n YOU WON \n");
+            YOUR_SCORE++;
         }
 
         else if (me == PAPER && computer == ROCK)
         {
             printf("\nYour choice is paper ,Computer choice is rock  **paper beats rock ");
             printf("\n YOU WON \n");
+            YOUR_SCORE++;
         }
         else if (me == PAPER && computer == SCISSOR)
         {
             printf("\nYour choice is paper ,Computer choice is scissor  **scissor beats paper ");
-            printf("\n  COMPUTER WON \n");
+            printf("\n  YOU LOSS \n");
+            COMPUTER_SCORE++;
         }
         else if (me == SCISSOR && computer == ROCK)
         {
             printf("\nYour choice is scissor ,Computer choice is rock  **rock beats scissor ");
-            printf("\n COMPUTER WON \n");
+            printf("\n YOU LOSS \n");
+            COMPUTER_SCORE++;
         }
         else if (me == SCISSOR && computer == PAPER)
         {
             printf("\nYour choice is scissor ,Computer choice is paper  **scissor beats paper ");
             printf("\n YOU WON \n");
+            YOUR_SCORE++;
         }
         else
         {
             printf(" \n GAME DRAW \n");
         }
+        printf("\n|---------------------------------------|");
+        printf("\n|           FINAL SCORE                 |");
+        printf("\n|---------------------------------------|");
+        printf("\n|YOUR SCORE IS %d | COMPUTER SCORE IS %d  |",YOUR_SCORE,COMPUTER_SCORE);
+        printf("\n|---------------------------------------|");
     }
 
     return 0;
