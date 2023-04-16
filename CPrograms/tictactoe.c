9d3759cced3c5450edfbbe8567dcd0887abe73db
alishatarika <alisha.tarikaa@gmail.com>
2023-04-16 19:42:55 +0530

    addgame

--- CPrograms/tictactoe.c
+++ CPrograms/tictactoe.c
@@ -16,7 +16,7 @@ int main()
    {
       playerboard();
       player = (player % 2) ? 1 : 2;
-      printf("player %d ,entr a number", player);
+      printf("player %d ,entr a number  ", player);
       scanf("%d", &choice);
       mark = (player == 1) ? 'x' : 'o';
       markingboard(choice, mark);
@@ -26,7 +26,7 @@ int main()
    } while (i == -1);
        playerboard();
    if (i == 1)
-      printf("player %d is winner ", --player);
+      printf("player %d is winner  ", --player);
    else
       printf("draw");
 }
@@ -37,12 +37,12 @@ void playerboard()
    printf("**TIC TAC TOE **\n player 1(X) ---player 2(o)\n");
    printf("   |   |   | \n");
    printf(" %c | %c | %c |\n", board[1], board[2], board[3]);
-   printf("___|____|____|___\n");
-   printf("   |   |   | \n");
-   printf(" %c  | %c | %c |\n", board[4], board[5], board[6]);
    printf("___|___|___|___\n");
    printf("   |   |   | \n");
-   printf(" %c  | %c | %c |\n", board[7], board[8], board[9]);
+   printf(" %c | %c | %c |\n", board[4], board[5], board[6]);
+   printf("___|___|___|___\n");
+   printf("   |   |   | \n");
+   printf(" %c | %c | %c |\n", board[7], board[8], board[9]);
    printf("___|___|___|_____\n");
 }
 void markingboard(int choice, char mark)
@@ -88,7 +88,7 @@ int checkforwin()
       return 1;
    else if (board[3] == board[5] && board[5] == board[7])
       return 1;
-   else if (board[1] != '1' && board[2] != '2' && board[3] != '3' && board[4] != '4', board[5] != '5' && board[6] != '6' && board[7] != '7' && board[8] != '8' && board[9] != '9')
+   else if (board[1] != '1' && board[2] != '2' && board[3] != '3' && board[4] != '4'&& board[5] != '5' && board[6] != '6' && board[7] != '7' && board[8] != '8' && board[9] != '9')
       return 0;
    else
       return -1;
