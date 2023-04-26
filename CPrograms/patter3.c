aae92905d518b9d463fef0338d44afac8937b305
alishatarika <alisha.tarikaa@gmail.com>
2023-04-26 21:28:41 +0530

    addprogram

--- CPrograms/patter3.c
+++ CPrograms/patter3.c
@@ -1,35 +1,42 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-  
-  
-   
-  for(int i =5;i>=1;i--)
+
+  // 123456789
+  // 1234 6789
+  // 123   789
+  // 12     89
+  // 1       9
+  int w=5;
+int y;
+  for (int i = 5; i >= 1; i--)
   {
-int count=1;
-int y=i;
-    for(int r=1;r<=i;r++)
+    int count = 1;
+
+    for (int r = 1; r <= i; r++)
     {
-      if(r==5)
+      if (r == 5)
       {
         continue;
       }
-      
-    printf("%d",count);
-    count++;
+
+      printf("%d", count);
+      count++;
     }
-    
-    for(int j=1;j<=(2*(5-i))-1;j++){
+
+    for (int j = 1; j <= (2 * (5 - i)) - 1; j++)
+    {
       printf(" ");
     }
+    y=w;
     for(int k=1;k<=i;k++)
     {
       printf("%d",y);
-      y--;
+      y++;
     }
+    w++;
     printf("\n");
   }
- 
-  
-    return 0;
+
+  return 0;
 }
